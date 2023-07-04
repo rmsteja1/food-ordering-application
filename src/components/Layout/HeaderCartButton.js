cedba36cec82ff7a07ab2a80921d3848bd604221
rmsteja1 <teja.ramisetty@sjsu.edu>
2023-07-03 20:13:20 -0700

    Added logic for dynamically updating cart item count. Added dynamic graphics for button bump action.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,39 @@
-import React, { Fragment } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
-const HeaderCartButton = () => {
+const HeaderCartButton = (props) => {
+  const cartCntx = useContext(CartContext);
+  const [isButtonHighlighted, setButtonHighlighter] = useState(false);
+  const numberOfCartItems = cartCntx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+  const { items } = cartCntx;
+
+  useEffect(() => {
+    if (items.length == 0) {
+      return;
+    }
+    setButtonHighlighter(true);
+    const timer = setTimeout(() => {
+      setButtonHighlighter(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
+  let btnClasses = `${classes.button} ${
+    isButtonHighlighted ? classes.bump : ""
+  }`;
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}> 3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
